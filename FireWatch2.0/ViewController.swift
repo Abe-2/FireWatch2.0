c7032b81d3bd1196f0c28df5b95368314884f29a
abe-2 <agg2@outlook.com>
2019-10-18 08:46:06 +0300

    Initial Commit

---
+++ FireWatch2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireWatch2.0
+//
+//  Created by Abdalwahab on 10/18/19.
+//  Copyright © 2019 team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
